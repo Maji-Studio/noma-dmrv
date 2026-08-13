db60c45c7c951fa8b391e22ee6b51a0856fad187
Kenji Nguyen <kenji@maji.studio>
2026-08-13 20:26:00 +0200

    chore: squash storage location migrations

--- drizzle/0107_mushy_cerebro.sql
+++ drizzle/0107_mushy_cerebro.sql
@@ -48,14 +48,14 @@ CREATE TABLE "certifier_storage_locations" (
 	"created_at" timestamp DEFAULT now() NOT NULL,
 	"updated_at" timestamp DEFAULT now() NOT NULL,
 	CONSTRAINT "certifier_storage_locations_id_organization_id_unique" UNIQUE("id","organization_id"),
-	CONSTRAINT "certifier_storage_locations_provider_customer_location_unique" UNIQUE("provider","customer_location_id"),
+	CONSTRAINT "certifier_storage_locations_provider_project_location_unique" UNIQUE("provider","external_project_id","customer_location_id"),
 	CONSTRAINT "certifier_storage_locations_provider_reference_unique" UNIQUE("provider","supplier_reference"),
 	CONSTRAINT "certifier_storage_locations_provider_external_id_unique" UNIQUE("provider","external_storage_location_id"),
 	CONSTRAINT "certifier_storage_locations_provider_is_isometric" CHECK ("certifier_storage_locations"."provider" = 'isometric')
 );
 --> statement-breakpoint
 ALTER TABLE "certifier_production_batches" ADD CONSTRAINT "certifier_production_batches_id_organization_id_unique" UNIQUE("id","organization_id");--> statement-breakpoint
-CREATE UNIQUE INDEX "customer_locations_id_organization_id_unique" ON "customer_locations" USING btree ("id","organization_id");--> statement-breakpoint
+ALTER TABLE "customer_locations" ADD CONSTRAINT "customer_locations_id_organization_id_unique" UNIQUE("id","organization_id");--> statement-breakpoint
 ALTER TABLE "certifier_biochar_applications" ADD CONSTRAINT "certifier_biochar_applications_organization_id_organizations_id_fk" FOREIGN KEY ("organization_id") REFERENCES "public"."organizations"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "certifier_biochar_applications" ADD CONSTRAINT "certifier_biochar_applications_application_id_organization_id_applications_id_organization_id_fk" FOREIGN KEY ("application_id","organization_id") REFERENCES "public"."applications"("id","organization_id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "certifier_biochar_applications" ADD CONSTRAINT "certifier_biochar_applications_credit_batch_id_organization_id_credit_batches_id_organization_id_fk" FOREIGN KEY ("credit_batch_id","organization_id") REFERENCES "public"."credit_batches"("id","organization_id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
--- drizzle/0110_watery_nomad.sql
+++
@@ -1,2 +0,0 @@
-DROP INDEX "customer_locations_id_organization_id_unique";--> statement-breakpoint
-ALTER TABLE "customer_locations" ADD CONSTRAINT "customer_locations_id_organization_id_unique" UNIQUE("id","organization_id");
