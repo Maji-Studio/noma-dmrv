d2cadef1a56b22a0ec02c70648723645b0d5098a
Kenji Nguyen <kenji@maji.studio>
2026-08-15 23:51:30 +0200

    chore: consolidate removal attribution migration

--- drizzle/0111_sticky_rictor.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE "credit_batches" DROP CONSTRAINT "credit_batches_removal_id_certifier_removals_id_fk";
---> statement-breakpoint
-DROP INDEX "credit_batches_removal_id_idx";--> statement-breakpoint
-ALTER TABLE "credit_batches" DROP COLUMN "removal_id";
